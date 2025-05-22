db4975a936d449de5704892b11de8b51713c5595
tsakshi01 <tsakshi01@gmail.com>
2025-05-21 18:05:14 -0700

    Added Edits

--- src/components/EditorLetter.js
+++ src/components/EditorLetter.js
@@ -4,34 +4,31 @@ import Background from "../images/ArticleLetterBackground.png";
 import { mediaQueries } from "../shared/config";
 
 const OuterContainer = styled.div`
-    background: url(${Background});
-    width: 100%;
-    height: fit-content;
-    padding: 20%;
-    background-repeat: no-repeat;
-    background-size: cover;
+    background: url(${Background}) no-repeat center center;
+    width: 100%; 
+    height: 100%;
+    min-height: 100vh;
+    background-size: contain;
 `;
 
-
-//background: url(${Background});
 
 const Container = styled.div`
     border-radius: 4vh;
     width: 50%;
-    height: 100%;
     background: rgba(255, 255, 255, 0.12);
     backdrop-filter: blur(12px);
     -webkit-backdrop-filter: blur(12px);
     border: 1.5px solid rgba(255, 255, 255, 0.3);
     color: white;
-    padding: 3vh 3.5vh 2vh 3.5vh; 
     display: flex;
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    margin-left: 30%;
 
   ${mediaQueries.mobile} {
-    width: 80%;
+    width: 70%;
+    margin-left: 10%;
   }
 `;
 
@@ -42,27 +39,10 @@ const Text = styled.div`
     line-height: 30px;
     letter-spacing: 0%;
     vertical-align: middle;
+    padding: 2em;
     ${mediaQueries.mobile} {
         padding: 1em 1em;
     }
-`;
-
-const Content = styled.div`
-  margin: 5%;
-  margin-top: 0%;
-  margin-bottom: 7%;
-  font-family: "PT Sans", sans-serif;
-  font-optical-sizing: auto;
-  font-weight: 400;
-  font-style: normal;
-  font-size: 20px;
-  line-height: 21.1px;
-  text-align: justify;
-  color: #F6E4E1;
-  ${mediaQueries.mobile} {
-    font-size: 14px;
-    
-  }
 `;
 
 export default function EditorLetter(props) {
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,12 +3,8 @@ import styled from "styled-components";
 import SciHealthLanding from "../images/SciHealthLanding.gif";
 
 const Container = styled.div`
-//   width: 100%;
-//   height: 100em;
-//   margin: auto;
-    width: 1440px;
+  width: 100%;
   height: 850px;
-  margin: 25px auto 0 auto;
   display: flex;
   position: relative;
   flex-direction: column;
@@ -16,6 +12,7 @@ const Container = styled.div`
   align-items: center;
   background: url(${SciHealthLanding}) no-repeat center center;
   background-size: cover;
+  background-color: black;
 `;
 
 const Landing = () => {
